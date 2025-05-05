3acab50d2aed82c6ec228a599ab4ebad06e490cb
PierreENOlivier <pierre.olivier.eloi@gmail.com>
2025-05-05 14:42:42 +0200

    Day 4, Day 5 - Highest score loop

--- 100DaysOfCode/Day 4/Rock Paper Scissors/task.py
+++ 100DaysOfCode/Day 4/Rock Paper Scissors/task.py
@@ -24,3 +24,45 @@ scissors = '''
       (____)
 ---.__(___)
 '''
+
+from random import randint, choice
+game_art = [rock, paper, scissors]
+possible_answers = range(3)
+
+play_status = True
+while play_status:
+	
+	ans_player = int(input("What do you choose? Type the number (0) Rock, (1) Paper, (2) Scissors\n"))
+	
+	
+	if ans_player not in possible_answers:
+		print("Error! Not a valid entry!")
+		exit()
+	else:
+		print(game_art[ans_player])
+	
+	ans_machine = choice(possible_answers)
+	print(f"Machine played ({ans_machine}):\n")
+	print(game_art[ans_machine])
+	
+	if ans_player == 0 and ans_machine == 2:
+		print("You win!")
+	elif ans_player == 2 and ans_machine == 0:
+		print("You lose!")
+	elif ans_player < ans_machine and abs(ans_player - ans_machine) == 1:
+		print("You lose!")
+	elif ans_player > ans_machine and abs(ans_player - ans_machine) == 1:
+		print("You win!")
+	elif ans_player == ans_machine:
+		print("It's a draw!")
+		
+	
+	ans_continue = input("Play another round? (y)es / (n)o \n").lower()
+	if ans_continue not in ['y', 'n']:
+		print('Invalid unit!')
+	elif ans_continue == "n":
+		print("Exiting the game!")
+		play_status = False
+	else:
+		continue
+			
--- 100DaysOfCode/Day 5/Highest Score/task.py
+++ 100DaysOfCode/Day 5/Highest Score/task.py
@@ -1,2 +1,22 @@
 student_scores = [150, 142, 185, 120, 171, 184, 149, 24, 59, 68, 199, 78, 65, 89, 86, 55, 91, 64, 89]
 print(range(1, 10))
+
+print(f"with sum function {sum(student_scores)}")
+
+total_sum = 0
+for score in student_scores:
+	total_sum += score
+
+print(f"For loop sum: {total_sum}")
+
+# Get max
+
+print(f"Max func: {max(student_scores)}")
+
+current_max = 0
+for score in student_scores:
+	# print(f"Score: {score}")
+	if score > current_max:
+		current_max = score
+
+print(f"Max Loop: {current_max}")
